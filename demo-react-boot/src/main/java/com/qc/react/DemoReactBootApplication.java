5e4c6c8226288d468e2e00f5255e0b97d2d9e787
dipin.kerketta <dipin.kerketta@gmail.com>
2020-02-06 22:29:42 +0530

    spring application without react

---
+++ demo-react-boot/src/main/java/com/qc/react/DemoReactBootApplication.java
@@ -0,0 +1,13 @@
+package com.qc.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReactBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReactBootApplication.class, args);
+	}
+
+}
