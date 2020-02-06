21a5ea9fb3db4f9fec29e37587aac9c52a4f7ea4
dipin.kerketta <dipin.kerketta@gmail.com>
2020-02-06 22:11:17 +0530

    spring boot react poc

---
+++ src/test/java/com/qc/react/DemoReactBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qc.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReactBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
